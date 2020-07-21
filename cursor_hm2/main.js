da647d4ff33e0b002901777a50fcf9cb2dad21bd
SlavikLesyk <slaviklesyk706@gmail.com>
2020-07-21 12:32:09 +0300

    change while check to if check

--- cursor_hm2/main.js
+++ cursor_hm2/main.js
@@ -3,18 +3,26 @@ let currentNumber = null;
 
 let firstNumber = +prompt('Введіть перше ціле число N');
 
-while(isNaN(firstNumber) || !Number.isInteger(firstNumber)){
-    firstNumber =  +prompt('Ви ввели невірне число N, повторіть спробу');
-
+while(true){
+    if (isNaN(firstNumber)){
+        firstNumber = +prompt('Ви ввели стрічку, введіть число N');
+    } else if (!Number.isInteger(firstNumber)){
+        firstNumber = +prompt('Число має бути цілим, введіть число N ще раз')
+    } else 
+        break;       
 }
 
 let secondNumber = +prompt('Введіть друге ціле число M');
 
-while(isNaN(secondNumber) || !Number.isInteger(secondNumber) || firstNumber > secondNumber){
-    firstNumber < secondNumber ?
-        secondNumber =  +prompt('Ви ввели невірне число M, повторіть спробу'):
-        secondNumber =  +prompt('Ви ввели M < N, повторіть спробу');
-
+while(true){
+    if (isNaN(secondNumber)){
+        secondNumber = +prompt('Ви ввели стрічку, введіть число M');
+    } else if (!Number.isInteger(secondNumber)){
+        secondNumber = +prompt('Число має бути цілим, введіть число M ще раз')
+    } else if (firstNumber > secondNumber){
+        secondNumber = +prompt('Число M має бути більшим за N, введіть число M ще раз')
+    }else
+        break;      
 }
 
 const onlyOdd = confirm('Пропускати парні числа?');
